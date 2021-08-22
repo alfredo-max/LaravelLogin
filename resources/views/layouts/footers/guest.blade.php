a26462aed7fb656182710870fd073da2b88d515d
alfredo-max <almalfredo19@gmail.com>
2021-08-22 07:14:43 -0500

    v1

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, made by
+        <a href="#" target="_blank">Alfredo Almarales</a> 
+        </div>
+    </div>
+</footer>
